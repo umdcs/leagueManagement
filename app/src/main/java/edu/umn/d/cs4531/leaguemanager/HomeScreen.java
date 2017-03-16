01d1b51f482055326361d70a237380346b4ab952
Lardy016 <lardy016@d.umn.edu>
2017-03-16 16:27:25 -0500

    Added some interface for MVP, incomplete

--- app/src/main/java/edu/umn/d/cs4531/leaguemanager/HomeScreen.java
+++ app/src/main/java/edu/umn/d/cs4531/leaguemanager/HomeScreen.java
@@ -3,11 +3,13 @@ package edu.umn.d.cs4531.leaguemanager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class HomeScreen extends AppCompatActivity {
+public class HomeScreen extends AppCompatActivity implements MVPComponents.View {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
     }
+
+
 }
