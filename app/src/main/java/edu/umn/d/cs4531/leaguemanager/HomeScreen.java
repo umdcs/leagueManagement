01a5a4237283d16ec70dd319d1ee2669bad24dfd
Jeff Vang <Vang1792@d.umn.edu>
2017-03-22 23:42:34 -0500

    Uncommented home screen spinner to retrieve information from dummy leagues rather than variables created in home screen.

--- app/src/main/java/edu/umn/d/cs4531/leaguemanager/HomeScreen.java
+++ app/src/main/java/edu/umn/d/cs4531/leaguemanager/HomeScreen.java
@@ -65,11 +65,11 @@ public class HomeScreen extends AppCompatActivity implements MVPComponents.View 
 
     private void setupTeamSpinner() {
 
-       // ArrayList<String> teamArray = mPresenter.getTeams();
-        ArrayList<String> teamArray = new ArrayList<String>();
-        teamArray.add("Team1");
-        teamArray.add("Team2");
-        teamArray.add("Team3");
+        ArrayList<String> teamArray = mPresenter.getTeams();
+       // ArrayList<String> teamArray = new ArrayList<String>();
+        //teamArray.add("Team1");
+        //teamArray.add("Team2");
+        //teamArray.add("Team3");
 
 
         Spinner teamSpinner = (Spinner) findViewById(R.id.teamSpinner);
